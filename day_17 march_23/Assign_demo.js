d897ec4bb3b2dbdf55efa1777ffc97cc4211ea94
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-03-23 09:42:54 +0530

    day 17

---
+++ day_17 march_23/Assign_demo.js
@@ -0,0 +1,10 @@
+let obj1 = { a: 10, b: 20 };
+
+let obj2 = { c: 30, d: 40 };
+let obj3 = { e: 50, f: 60 };
+
+Object.assign(obj1, obj2, obj3)
+
+console.log(obj1)
+console.log(obj2)
+console.log(obj3)
