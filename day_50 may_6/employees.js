7d445fd2b933a710ce150269f28444078d15b06c
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-05-06 09:43:44 +0530

    swagger

--- day_50 may_6/employees.js
+++ day_50 may_6/employees.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 const { MongoClient } = require('mongodb');
 
 const client = new MongoClient('mongodb://0.0.0.0:27017');
@@ -10,7 +13,12 @@ async function connectToDB() {
     console.log('Connected successfully to mongoDB');
 }
 
-app.get('/employees', async (req, res) => {
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+app.get('/api/employees', async (req, res) => {
     try {
         const employees = await client.db('march_4_2024').collection('employees').find({}).toArray();
         res.json(employees);
@@ -18,7 +26,7 @@ app.get('/employees', async (req, res) => {
         console.log(err)
     }
 });
-app.get('/employees/:id', async (req, res) => {
+app.get('/api/employees/:id', async (req, res) => {
     const employeeId = +req.params.id;
     try {
         const employees = await client.db('march_4_2024').collection('employees').find({ eId: employeeId }).toArray();
@@ -27,16 +35,16 @@ app.get('/employees/:id', async (req, res) => {
         console.log(err)
     }
 });
-app.post('/employees', async (req, res) => {
+app.post('/api/employee', async (req, res) => {
     const payload = req.body;
     try {
         await client.db('march_4_2024').collection('employees').insertOne(payload);
-        res.send('Employee Added Successfully');
+        res.status(201).send('Employee Added Successfully');
     } catch (err) {
         console.log(err)
     }
 });
-app.patch('/employees/:id', async (req, res) => {
+app.patch('/api/employees/:id', async (req, res) => {
     const employeeId = +req.params.id;
     const payload = req.body;
     try {
@@ -46,7 +54,7 @@ app.patch('/employees/:id', async (req, res) => {
         console.log(err)
     }
 });
-app.delete('/employees/:id', async (req, res) => {
+app.delete('/api/employees/:id', async (req, res) => {
     const employeeId = +req.params.id;
     try {
         await client.db('march_4_2024').collection('employees').deleteOne({ eId: employeeId });
