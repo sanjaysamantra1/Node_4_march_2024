3063656bb33b8c13f719d492d34f3e65eecc7d8b
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-05-10 09:40:13 +0530

    jwt token

--- day_53 may_9/controller/AuthController.js
+++ day_53 may_9/controller/AuthController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 const UserModel = require('../model/UserSchema');
 const authRouter = express.Router();
 
@@ -18,11 +19,38 @@ authRouter.post('/login', async (req, res) => {
         let user = await UserModel.findOne({ email: req.body.email });
         if (!user) {
             res.send({ auth: false, msg: 'User Not found, Please register First' })
-        }else{
-            res.send('user found')
+        } else {
+            let isPasswordValid = bcryptjs.compareSync(req.body.password, user.password);
+
+            if (!isPasswordValid) {
+                res.send({ auth: false, msg: 'Invalid password' })
+            } else {
+                const expiryTime = 60;
+                const token = jwt.sign({ id: user._id }, 'MySecretKey', { expiresIn: expiryTime });
+                res.send({ auth: true, token: token, expiresIn: expiryTime })
+            }
         }
     } catch (err) {
         res.send({ auth: false, msg: 'Error While Logging in' })
+    }
+});
+authRouter.get('/userInfo', async (req, res) => {
+    console.log('userInfo route')
+    const token = req.headers['my-token'];
+    if (!token) {
+        res.send({ auth: false, msg: 'No Token Found, please pass token' })
+    } else {
+
+        jwt.verify(token, 'MySecretKey', async function (err, response) {
+            if (err) {
+                res.send({ auth: false, msg: 'Invalid Token' })
+            }
+            if(response){
+                console.log(response)
+                let userInfo = await UserModel.findById(response.id);
+                res.send(userInfo);
+            }
+        })
     }
 });
 
